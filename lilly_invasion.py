cb872ef98e6836f6370ae59281180fbb48c74f02
Rob8624 <robert_melen@yahoo.co.uk>
2023-01-08 11:26:31 +0000

    Initial commit

---
+++ lilly_invasion.py
@@ -0,0 +1,31 @@
+import pygame
+import sys
+from settings import Settings
+class LillyInvasion:
+
+    def __init__(self):
+        pygame.init()
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode((self.settings.screen_width,
+                                               self.settings.screen_height))
+        pygame.display.set_caption(self.settings.window_caption)
+
+
+    def run_game(self):
+        while True:
+            self._check_events_()
+            self._update_screen_()
+
+    def _check_events_(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT : sys.exit()
+
+    def _update_screen_(self):
+        self.screen.fill(self.settings.bf_color)
+        pygame.display.flip()
+
+
+if __name__ == "__main__":
+    li = LillyInvasion()
+    li.run_game()
+
